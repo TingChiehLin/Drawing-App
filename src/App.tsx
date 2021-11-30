f8d07a0a6c23b931cfa68fc441cfbd29d8a02907
Jay Lin <cooloojayoo@gmail.com>
2021-11-30 22:05:16 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./layout/Header";
+
+function App() {
+  return (
+    <div className="main-container">
+      <Header/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="h-16 w-full bg-blue-500 flex justify-center items-center">
+            <h1 className="text-lg">Drawing App</h1>
+        </div>
+    );
+};
+
+export default Header;
